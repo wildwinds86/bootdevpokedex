b2e9c96611920c6cef323eb01fd3c6389c7118fa
wildwinds86 <dclaydon@gmail.com>
2025-07-12 14:45:55 +0100

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wildwinds86/bootdevpokedex
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"time"
+
+	"github.com/wildwinds86/bootdevpokedex/internal/pokeapi"
+)
+
+func main() {
+	pokeClient := pokeapi.NewClient(5*time.Second, time.Minute*5)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
+}
